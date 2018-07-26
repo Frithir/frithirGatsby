1bf26e20e0babf7bd47e3e2d598e4336f1ce3343
Firthir <alexfrith@Alexs-MacBook-Pro.local>
2018-07-26 23:48:41 +1000

    debug config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,10 @@
 module.exports = {
   siteMetadata: {
     title: 'Frithir Software Development',
+    siteUrl: 'https://frithir.com',
+    email: 'alex@frithir.com',
+    phone: '',
+    address: 'Gold Coast, Australia',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -22,7 +26,7 @@ module.exports = {
         background_color: '#555',
         theme_color: '#555',
         display: 'standalone',
-        icon: '/static/favicon-128.png', // This path is relative to the root of the site.
+        icon: 'static/favicon-128.png', // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
