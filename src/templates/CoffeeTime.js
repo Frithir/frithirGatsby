ca12501ca246bb7cab2e5823917c07ea7c0698ac
alex frith <alex@raidn.co>
2022-05-24 23:11:07 +1000

    layouts

--- src/templates/CoffeeTime.js
+++ src/templates/CoffeeTime.js
@@ -4,7 +4,8 @@ export default () => (
   <div className="section cascade bg-pink">
     <div className="container">
       <p className="black">
-        I drink coffee, write code and improve my development skills every day
+        I drink coffee, write code and <br />
+        improve my development skills every day
       </p>
     </div>
   </div>
--- src/templates/Experience.js
+++ src/templates/Experience.js
@@ -5,9 +5,10 @@ export default () => (
     <div className="container">
       <p className="black">
         With more than 10 years of experience in digital development & hundreds
-        of websites developed.<span className="orange">
-          I write code that works™
-        </span>
+        <br />
+        of websites developed.
+        <br />
+        <span className="orange">I write code that works™</span>
       </p>
     </div>
   </div>
