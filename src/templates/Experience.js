32b17253ab1cab3bbfdda60f41062f1ca5267b5b
alex frith <alex@raidn.co>
2022-05-24 23:24:09 +1000

    layout

--- src/templates/Experience.js
+++ src/templates/Experience.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => (
   <div className="section cascade bg-cream">
     <div className="container">
-      <p className="black" style={{ width: '600px' }}>
+      <p className="black" style={{ width: '700px', margin: '0 auto' }}>
         With more than 15 years of experience in digital development & hundreds
         of websites developed.
       </p>
